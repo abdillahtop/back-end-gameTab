06fa71e01a1cd6dec8dd62228530795f4ce4f89d
abdilahtop <abdilahtop@gmail.com>
2019-08-13 19:18:20 +0700

    haha

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ require('dotenv').config() //initialize dotenv config
 
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'emotemysql.com',
+    host: process.env.DB_HOST || 'remotemysql.com',
     user: process.env.DB_USER || 'bfzxXkUDdw',
     password: process.env.DB_PASSWORD || 'zGjlbWkKyl',
     database: process.env.DB_NAME || 'bfzxXkUDdw'
